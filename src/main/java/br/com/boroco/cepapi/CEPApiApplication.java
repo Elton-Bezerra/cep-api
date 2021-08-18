3c2b1bbaadb49a1c3e3f5364bdc9b03d95e3a590
Elton Bezerra <elton.cin@hotmail.com>
2021-08-17 21:21:50 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/boroco/cepapi/CEPApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.boroco.cepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CEPApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CEPApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/boroco/cepapi/CEPApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boroco.cepapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CEPApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
